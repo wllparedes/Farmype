8d760cf099fd77affb68ea5e1997d1422bc96ad9
Walin Paredes <walinparedes3010@gmail.com>
2023-11-12 20:41:58 -0500

    add[seeder User]

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,8 @@
 
 namespace Database\Seeders;
 
+use App\Models\User;
+use Hash;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -13,6 +15,38 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+
+        $dataUsers = [
+            [
+                'names_surnames' => 'ADP Technology',
+                'document_type' => 'dni',
+                'document_number' => '74556779',
+                'role' => 'company',
+                'departament' => 'lima',
+                'province' => 'lima',
+                'district' => 'ate',
+                'address' => 'Los Olivos',
+                'email' => 'adp@gmail.com',
+                'phone' => '901901901',
+                'password' => Hash::make('adptechnology'),
+            ],
+            [
+                'names_surnames' => 'Carmen',
+                'document_type' => 'dni',
+                'document_number' => '10101010',
+                'role' => 'clients',
+                'departament' => 'lima',
+                'province' => 'lima',
+                'district' => 'ate',
+                'address' => 'San Juan de Lurigancho',
+                'email' => 'carmen@gmail.com',
+                'phone' => '902902902',
+                'password' => Hash::make('carmen'),
+            ]
+        ];
+
+        foreach ($dataUsers as $data) {
+            User::create($data);
+        }
     }
 }
