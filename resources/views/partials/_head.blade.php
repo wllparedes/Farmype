4c30f3c22f544b8e7145bb28479adcf6ad7ccc39
Wallace <walinparedes3010@gmail.com>
2024-06-10 00:58:54 -0500

    Update font styles and import in _head.blade.php and app.css

--- resources/views/partials/_head.blade.php
+++ resources/views/partials/_head.blade.php
@@ -8,12 +8,13 @@
     <!-- Favicon -->
     <link href="{{ asset('assets/img/brand/favicon.png') }}" rel="icon" type="image/png">
     <!-- Fonts -->
-    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Ubuntu:ital,wght@0,300;0,400;0,500;0,700;1,300;1,400;1,500;1,700&display=swap" rel="stylesheet">
     <!-- Icons -->
     <link href="{{ asset('assets/js/plugins/nucleo/css/nucleo.css') }}" rel="stylesheet" />
     <link href="{{ asset('assets/js/plugins/@fortawesome/fontawesome-free/css/all.min.css') }}" rel="stylesheet" />
     <!-- CSS Files -->
     <link href="{{ asset('assets/css/argon-dashboard.css?v=1.1.2') }}" rel="stylesheet" />
     <link rel="stylesheet" href="{{ asset('assets/css/app.css') }}">
+    
     @yield('optional_links')
 </head>
